5ddd671987e8681f034426c8df9c2e64eafc195e
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-09-02 22:49:30 +0600

    header & category added

---
+++ assets/app.js
@@ -0,0 +1,39 @@
+// variables
+const cateGoryWrapper = document.getElementById('category-list')
+
+// fetchingCategory list
+const fetchingCategoryList = async () =>{
+       try{
+        const url = 'https://openapi.programming-hero.com/api/news/categories'
+        const res = await fetch(url)
+        const data = await res.json()
+        displayCategoryList(data.data.news_category);
+       }
+       catch(err){
+            console.log(err);
+       } 
+}
+// displaying category list
+
+const displayCategoryList = async (categories) =>{
+    
+    
+    categories.forEach(category => {
+        const {category_name : categoryName , category_id : id} = category
+        const createLi = document.createElement('li')
+        createLi.onclick = () => displayPost(id) 
+        createLi.innerText = categoryName
+        createLi.classList.add('list-group-item' , 'semibold-text')
+        cateGoryWrapper.appendChild(createLi)
+         
+    })
+}
+
+// display post function 
+
+const displayPost = async (id) => {
+    
+}
+
+// calling function
+fetchingCategoryList()
